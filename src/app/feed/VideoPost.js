896cde99664a6cd5916035e5cd5c52db484cd96e
student <bitstudent@bgit.rs>
2018-03-27 15:35:01 +0200

    Implement feed page

---
+++ src/app/feed/VideoPost.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const VideoPost = (props) => {
+
+    return (
+        <div className="video-container">
+            <iframe title="video" width="95%" height="315" src="https://www.youtube.com/embed/riCf7peN80U" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen ></iframe>
+        </div>
+    )
+}
+
